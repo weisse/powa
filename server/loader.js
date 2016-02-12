eacb6b6d52d8ca5a76cf80f11bd195192efbbe63
weisse <pietro.cucinella@kubique.com>
2016-02-12 09:12:01 +0100

    modified:   package.json 	modified:   server/loader.js

--- server/loader.js
+++ server/loader.js
@@ -5,36 +5,36 @@ var bluebird = require("bluebird");
 var speedyStatic = require("speedy-static");
 
 module.exports = function(app, config){
-	
+
 	return new bluebird.Promise(function(res, rej){
-			
+
 		app.set("packageRootPath", config.packageRootPath);
 		app.set("allowedOrigin", config.allowedOrigin);
-		app.set("allowedMethods", congig.allowedMethods);
+		app.set("allowedMethods", config.allowedMethods);
 		app.set("allowedHeaders", config.allowedHeaders);
 		app.set("allowCredentials", config.allowCredentials);
-		
+
 		if(config.allowCors){
-			
+
 			app.use("/*", require(p.join(__dirname, "./middlewares/allowCors.js")));
 		    app.options("/*", require(p.join(__dirname, "./middlewares/options.js")));
-			
+
 		}
-		
+
 	    app.get("/satisfy/:package", require(p.join(__dirname, "./services/satisfy.js")));
 	    app.get("/satisfyWithAll/:package", require(p.join(__dirname, "./services/satisfyWithAll.js")));
-	    
+
 	    new bluebird.Promise(function(res, rej){
-			
+
 			res(speedyStatic(p.resolve(config.packageRootPath), config));
-			
+
 		}).then(function(middleware){
-			
+
 			app.use("/packages", middleware);
 			res(app);
-			
+
 		});
-		
+
 	});
-	
+
 }
